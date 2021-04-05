10e3a6314c5992c44d276f360f8b308d5d97f05b
syarifahaisha <aishageubrina@gmail.com>
2021-04-05 21:14:20 +0700

    read done minus jarak

--- src/read.py
+++ src/read.py
@@ -1,8 +1,4 @@
-import os
 
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
 
 # Membaca masukan dari file txt dan mengembalikannya 
 # dalam bentuk array
@@ -25,7 +21,7 @@ arrayOfWords = makeArrayofWords(lines)
 for i in range (len(arrayOfWords)):
     print(arrayOfWords[i])
 
-
+# Memisahkan matriks ketetanggaan dari input file
 def makeAdjancentMatrix(arrayOfWords):
     adjacentMatrix = []
     N = int(arrayOfWords[0][0])
@@ -33,8 +29,38 @@ def makeAdjancentMatrix(arrayOfWords):
         adjacentMatrix.append(arrayOfWords[i])
     return adjacentMatrix
 
+# Memisahkan matriks node dari input file
+def makeNodeMatrix(arrayOfWords):
+    nodeMatrix = []
+    N = int(arrayOfWords[0][0])
+    for i in range (1,N+1):
+        nodeMatrix.append(arrayOfWords[i])
+    return nodeMatrix
+
 print("")
 adjacentMatrix = makeAdjancentMatrix(arrayOfWords)
 for i in range (len(adjacentMatrix)):
     print(adjacentMatrix[i])
+
+print("")
+nodeMatrix = makeNodeMatrix(arrayOfWords)
+for i in range (len(nodeMatrix)):
+    print(nodeMatrix[i])
+
+#Membuat dictionary dari matrix dan tetangganya
+def makeDictionary(adjacentMatrix,nodeMatrix):
+    dictionary_adj = dict()
+    for i in range(len(adjacentMatrix)):
+        tempAdj = []
+        for j in range (len(adjacentMatrix)):
+            if (adjacentMatrix[i][j] != '0'):
+                tempAdj.append(nodeMatrix[j][0])
+        dictionary_adj.setdefault(nodeMatrix[i][0], tempAdj)
+    return dictionary_adj
+
+print("")
+dictionary_adj = makeDictionary(adjacentMatrix,nodeMatrix)
+for key, value in dictionary_adj.items():
+    print(key, " : ",value)
+
 
