3612ad528c6fda9329ef03a07d824c4a7c218870
auliaadila <auliaadila36@gmail.com>
2021-04-05 21:11:39 +0700

    ngasal dulu

--- src/read.py
+++ src/read.py
@@ -1,4 +1,8 @@
+import os
 
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
 
 # Membaca masukan dari file txt dan mengembalikannya 
 # dalam bentuk array
