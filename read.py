836adfe5300b91687978a092a37547680988c53d
syarifahaisha <68510449+syarifahaisha@users.noreply.github.com>
2021-04-05 20:28:12 +0700

    readfile setengah jadi

---
+++ read.py
@@ -0,0 +1,36 @@
+
+
+# Membaca masukan dari file txt dan mengembalikannya 
+# dalam bentuk array
+def readinput(filename):
+    file = open(filename)
+    lines = file.read().splitlines()
+    return lines
+
+# Mengubah array of strings menjadi array of words
+def makeArrayofWords(lines):
+    arrayOfWords = []
+    for line in lines:
+        line = line.split(' ')
+        arrayOfWords.append(line)
+    return arrayOfWords
+
+
+lines = readinput("itb.txt")
+arrayOfWords = makeArrayofWords(lines)
+for i in range (len(arrayOfWords)):
+    print(arrayOfWords[i])
+
+
+def makeAdjancentMatrix(arrayOfWords):
+    adjacentMatrix = []
+    N = int(arrayOfWords[0][0])
+    for i in range (N+1,(len(arrayOfWords))):
+        adjacentMatrix.append(arrayOfWords[i])
+    return adjacentMatrix
+
+print("")
+adjacentMatrix = makeAdjancentMatrix(arrayOfWords)
+for i in range (len(adjacentMatrix)):
+    print(adjacentMatrix[i])
+
